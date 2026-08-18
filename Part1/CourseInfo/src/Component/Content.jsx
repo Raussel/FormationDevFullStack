22ccf682ea0707be26d057a44f4677e6fb0414ce
Rausel <rousselvaldespounde@gmail.com>
2026-08-18 21:27:10 +0100

    Fin de la Partie 1b

--- Part1/CourseInfo/src/Component/Content.jsx
+++ Part1/CourseInfo/src/Component/Content.jsx
@@ -19,3 +19,4 @@ const Content = () => {
 };
 
 export default Content
+
--- Valdes/src/App.jsx
+++ Valdes/src/App.jsx
@@ -1,32 +1,48 @@
 const Hello = (props) => {
-   return(
+  return (
     <div>
-      <p> Hello {props.name},you are {props.age} years old</p>
+      <p>
+        {" "}
+        Hello {props.name},you are {props.age} years old
+      </p>
     </div>
-   )
-
-}
-
+  );
+};
 
 const Footer = () => {
   return (
     <div>
-      greeting app created by <a href="https://github.com/mluukkai">
-      </a>
+      greeting app created by <a href="https://github.com/mluukkai"></a>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const friends = ['Peter','Maya']
-  return(
+  const friends = ["Peter", "Maya"];
+  return (
     <div>
       <p>{friends}</p>
       <Hello />
-      <Footer/>
+      <Footer />
     </div>
+  );
+};
+
+
+
+
+// Les Fonctions
+
+const average = (a,b) => {
+  console.log(a)
+  console.log(b)
+  return(
+    (a+b)/2
   )
 }
- 
+
+const result = average(12.6,15)
+console.log(result)
+
 
 export default App
