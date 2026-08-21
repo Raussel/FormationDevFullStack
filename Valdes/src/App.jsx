35b8d887b3b9ce444744627dcc66169fa81bc8f5
Rausel <rousselvaldespounde@gmail.com>
2026-08-21 16:50:31 +0100

    Debut de la patie1c

--- Valdes/src/App.jsx
+++ Valdes/src/App.jsx
@@ -1,48 +1,13 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        {" "}
-        Hello {props.name},you are {props.age} years old
-      </p>
-    </div>
-  );
-};
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai"></a>
-    </div>
-  );
-};
+import {useState} from 'react-dom/client'
 
 const App = () => {
-  const friends = ["Peter", "Maya"];
-  return (
-    <div>
-      <p>{friends}</p>
-      <Hello />
-      <Footer />
-    </div>
-  );
-};
-
-
-
-
-// Les Fonctions
-
-const average = (a,b) => {
-  console.log(a)
-  console.log(b)
+  const[counter,setCounter ] = useState(0);
+  setTimeout(
+    () => setCounter(counter + 1),1000
+  )
   return(
-    (a+b)/2
+    <div>{counter}</div>
   )
 }
-
-const result = average(12.6,15)
-console.log(result)
-
 
 export default App
---
+++ Valdes/src/CourseInfo.jsx
@@ -0,0 +1,51 @@
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age;
+//   return (
+//     <div>
+//       <p>
+//         Hello {name},you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   );
+// };
+
+// const Footer = () => {
+//   return (
+//     <div>
+//       greeting app created by <a href="https://github.com/mluukkai"></a>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const name = "Peter";
+//   const age = 10;
+//   const object = {
+//     name: "Arto Hellas",
+//     age: 35,
+//   };
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//       <Hello name={object.name} age={object.age}/>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// // Les Fonctions
+
+// const average = (a, b) => {
+//   console.log(a);
+//   console.log(b);
+//   return (a + b) / 2;
+// };
+
+// const result = average(12.6, 15);
+// console.log(result);
+
+// export default App;
